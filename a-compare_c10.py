78e074424d44c4198b2f8176a8e6302a131cb5cd
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2023-06-02 11:46:18 +0200

    Compare-c10 plot.

--- a-compare_c10.py
+++ a-compare_c10.py
@@ -7,6 +7,10 @@ from torchosr.data import configure_division
 root= 'data'
 base = MNIST_base(root=root, download=True)
 
+green = 'forestgreen'
+orange = '#ED722E'
+blue = '#1E4CF5'
+
 # Prepare division
 config, openness = configure_division(base, 
                                       repeats=5, 
@@ -26,7 +30,8 @@ print(res.shape)
 print(res_m.shape)
 
 cols = ['blue', 'purple', 'red']
-fig, ax = plt.subplots(5, 4, figsize=(14,10), sharex=True, sharey=True)
+cols = [green, blue, orange]
+fig, ax = plt.subplots(5, 4, figsize=(7,7), sharex=True, sharey=True)
 
 for i in range(5):
     for m_id, m in enumerate(['Inner', 'Outer', 'Halfpoint', 'Overall']):
@@ -35,18 +40,21 @@ for i in range(5):
             ax[order[i], m_id].set_title(m)
             ax[-1, m_id].set_xlabel('epoch')
         if m_id==0:
-            ax[order[i], m_id].set_ylabel('WA \n Openness=%.3f'% openness[order[i]])
+            ax[order[i], m_id].set_ylabel('$O=%.3f$\nWeighted Acc.'% openness[order[i]], fontsize=10)
         
         for method_id, method in enumerate(['SM', 'OM', 'GSL']):
-            ax[order[i],m_id].plot(res_m[m_id, order[i], method_id], c=cols[method_id], label=method, alpha=0.6)
+            ax[order[i],m_id].plot(res_m[m_id, order[i], method_id], 
+                                   c=cols[method_id], 
+                                   label=method, alpha=0.6)
 
-        ax[order[i],m_id].grid(ls=':')
-
-ax[0,0].legend(ncols=3, frameon=False)
+ax[0,0].legend(ncols=1, frameon=False)
 
 for a in ax.ravel():
     a.spines['top'].set_visible(False)
     a.spines['right'].set_visible(False)
+    a.set_xlim(0, 128)
+    a.set_yticks(np.linspace(0,1,5))
+    a.grid(ls=":")
 
 plt.tight_layout()
 plt.savefig('foo.png')
